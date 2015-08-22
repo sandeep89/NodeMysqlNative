85505bd640e5cdea4f7f3e6433b9e602af46be20
Sandeep Pandey <sandepan@cisco.com>
2015-08-22 09:35:35 +0530

    Added support for restify

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+var restify = require("restify");
 var middleWares = require("./lib/middlewares")
 
+var server = restify.createServer();
 var employeeService = middleWares.services ? middleWares.services.employee : {};
 
-employeeService.addEmployee("Sandeep", "Pandey", "trainer", "sanpan", function(err, row){
+/*employeeService.addEmployee("Sandeep", "Pandey", "trainer", "sanpan", function(err, row){
     if(err){
         console.log("error while inserting record", err)
     }else{
         console.log(row);
     }
+})*/
+
+server.get("/employees", function(req, res, next){
+    employeeService.fetch(function(err, rows){
+        if(err){
+            res.send(err);
+            next()
+        }else{
+            res.send(rows);
+            next();
+        }
+    });
 })
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
